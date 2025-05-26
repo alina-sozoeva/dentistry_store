5730dca40df4c6f75e09bf604f1d19188ab48e51
alina-sozoeva <sozoeva.alina@gmail.com>
2025-05-26 10:11:34 +0600

    init project

---
+++ src/router/router.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter, Navigate, Outlet } from "react-router-dom";
+import * as Pages from "../pages";
+import { Footer, Header } from "../components";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Navigate to="/products" /> },
+      { path: "/home", element: <Pages.HomePage /> },
+      { path: "/products", element: <Pages.ProductsPage /> },
+    ],
+  },
+]);
